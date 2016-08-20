ac7f33bd390375ca8125d1d4491c0813d54f3839
Simon <oneSimon9398@gmail.com>
2016-08-20 19:54:40 +0800

    使用MongoDB的云服务

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@
 
 var mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/iTodo", function (error) {
+mongoose.connect("mongodb://one:zxc123@ds013216.mlab.com:13216/itodo", function (error) {
 	if (error) {
 		console.log('connect fail to database !');
 		console.error(error);
