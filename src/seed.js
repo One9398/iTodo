3fb798e2719a4968a347612dabb114488101ea35
Simon <oneSimon9398@gmail.com>
2016-08-20 18:15:14 +0800

    添加mongodb 包

--- src/seed.js
+++ src/seed.js
@@ -1,17 +1,17 @@
 'use strict';
 var Todo = require("./models/todo.js");
 
-// var todos = [
-// 	"写文章",
-// 	"写代码",
-// 	"跑步",
-// 	"哈哈哈"
-// ];
+var todos = [
+	"写文章",
+	"写代码",
+	"跑步",
+	"哈哈哈"
+];
 
-// todos.forEach(function (todo, index) {
-// 	Todo.find({'name': todo}, function(error, todos) {
-// 		if (!error && !todos.length) {
-// 			Todo.create({completed: false, name: todo});
-// 		}		
-// 	});
-// })
+todos.forEach(function (todo, index) {
+	Todo.find({'name': todo}, function(error, todos) {
+		if (!error && !todos.length) {
+			Todo.create({completed: false, name: todo});
+		}		
+	});
+})
